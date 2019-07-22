b076000109f9fd07986ea4d001bda731ea346ace
peterennis <peterennis@yahoo.com>
2019-07-22 16:38:55 -0700

    fix(test): to allow any element add no-errors-schema to component

--- projects/iconify/src/lib/icon.module.ts
+++ projects/iconify/src/lib/icon.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IconDirective } from './icon.directive';
 
 
 @NgModule({
   declarations: [IconDirective],
   imports: [],
-  exports: [IconDirective]
+  exports: [IconDirective],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IconModule {
 }
