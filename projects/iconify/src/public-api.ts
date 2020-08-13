61baf4993767ef063286ab629e56817ead27c206
Mark Lagendijk <mark@lagendijk.nl>
2020-08-13 15:31:05 +0200

    fix: export IconService
    IconService is currently not exported, making it impossible to register icons.

--- projects/iconify/src/public-api.ts
+++ projects/iconify/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/icon.directive';
 export * from './lib/icon.module';
 export * from './lib/icon.interface';
 export * from './lib/icon-props.interface';
+export * from './lib/icon.service';
